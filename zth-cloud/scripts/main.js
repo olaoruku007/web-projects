4b7f77ecd901ba753b8d3ed01991233d7f0b09b5
Ola Oruku <olaoruku007@gmail.com>
2023-08-16 04:55:12 -0400

    zth web development

---
+++ zth-cloud/scripts/main.js
@@ -0,0 +1,12 @@
+// Image switcher code
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/programming.jpg") {
+    myImage.setAttribute("src", "images/devops.png");
+  } else {
+    myImage.setAttribute("src", "images/programming.jpg");
+  }
+    
+};
